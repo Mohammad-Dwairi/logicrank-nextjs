d5d69a784e0a60ddd3523c53f923aa007dd15d91
Mohammad Dwairi <mdwairy@gmail.com>
2022-06-24 20:07:06 +0300

    removed payment options from user options

--- utils/UserBadgePopoverOptions.js
+++ utils/UserBadgePopoverOptions.js
@@ -1,8 +1,8 @@
 
 export const userBadgePopoverOptions = [
     { label: 'Profile Settings', link: '/profile' },
-    { label: 'Payments Methods', link: '/account/payment' },
-    { label: 'Purchase History', link: '/account/purchase' },
+    // { label: 'Payments Methods', link: '/account/payment' },
+    // { label: 'Purchase History', link: '/account/purchase' },
     { label: 'Create Room', link: '/room/new' },
     { label: 'My Rooms', link: '/rooms' },
     { label: 'Logout', link: '/logout' },
